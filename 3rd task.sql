57fc2846a1e42bdecfc4e70917c7f9d42c94c7b6
Nikita <n.solgalov@mail.ru>
2023-03-04 21:15:09 +0300

    init project

---
+++ 3rd task.sql
@@ -0,0 +1,9 @@
+CREATE EXTENSION postgis;
+
+ALTER TABLE geo_test.test_point 
+	ADD COLUMN geom geometry(PointZ);
+	
+UPDATE geo_test.test_point
+	SET geom = ST_SetSRID(ST_MakePoint(x, y, z), 4326);
+
+SELECT ST_AsGeoJSON(geom) from geo_test.test_point;
